07add9e8519a8d7a94d5ca5b110347ad5bcb5a82
Nathanael Vieira <nathanael@f1sales.com.br>
2022-07-11 18:10:02 -0300

    Update all files using Rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_083453) do
-
-  create_table "heroes", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "token", null: false
-    t.index ["token"], name: "index_heroes_on_token"
+ActiveRecord::Schema.define(version: 20_220_616_083_453) do
+  create_table 'heroes', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'token', null: false
+    t.index ['token'], name: 'index_heroes_on_token'
   end
-
 end
